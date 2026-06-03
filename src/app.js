2feb2b6356d03a826379b8a76548dac87ad43f5d
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-03 20:19:03 +0530

    feat: add auth controller and register endpoint integration

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express'); 
+/* require all the routes here */
+  const authRouter = require('./routes/auth.routes');
 
-const app = express();
+  const app = express();
+  app.use(express.json());
 
 
+  /*using all the routes here */
+  app.use('/api/auth', authRouter);
 
 module.exports = app; 
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const authRouter = express.Router(); 
+const authController = require('../controllers/auth.controller');
+
+
+
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user
+ * @access Public
+ */
+
+authRouter.post('/register', authController.registerUserController); 
+
+
+
+module.exports = authRouter; 
