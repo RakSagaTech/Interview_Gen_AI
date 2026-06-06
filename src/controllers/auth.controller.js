dd4ed39b071fcf32bf66e0925c6d48e62adf48a3
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-06 16:05:24 +0530

    feat(auth): implement user registration with JWT authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const userModel = require('../models/user.model'); 
+
 
 
 /**
@@ -8,8 +11,48 @@ const userModel = require('../models/user.model');
  */
 async function registerUserController(req, res){
 
+  const {username, email, password} = req.body; 
+
+  if(!username || !email || !password){
+    return res.status(400).json({message: 'Please provide all required fields'});
+  }
+
+  const isUserAlreadyRegistered = await userModel.findOne({
+    $or:[
+      {username},
+      {email}
+    ]
+  })
+
+  if (isUserAlreadyRegistered){
+  return res.status(400).json({message: 'User with the same username or email already exists'})
+}
+
+const hashPassword = await bcrypt.hash(password, 10); 
+
+const newUser = await userModel.create({
+  username,
+  email,
+  password: hashPassword
+})
+  
+const token = jwt.sign({
+  id: newUser._id, 
+  username: newUser.username
+}, process.env.JWT_SECRET, {expiresIn: '1d'})
+
+res.cookie('token', token)
+
+return res.status(201).json({
+  message: 'User registered successfully',
+  user: {
+    id: newUser._id,
+    username: newUser.username, 
+    email: newUser.email
+  }
+})  
 }
 
 
 
-module.exports = {registerUserController};
+module.exports = {registerUserController}
