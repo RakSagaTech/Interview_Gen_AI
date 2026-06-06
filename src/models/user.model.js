8fd455e46c135a2ef8828c53c299a87783681b23
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-06 16:45:16 +0530

    feat(auth): add token blacklist model for JWT revocation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
 })
 
 
-const userModel = mongoose.model('User', userSchema); 
+const userModel = mongoose.model('user', userSchema); 
 
 
 module.exports = userModel;
