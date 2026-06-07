72e5bdd298409cc2f64a1487a88a56e5068c14c5
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-07 16:08:00 +0530

    feat(auth): implement register form with SCSS styling

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,7 +1,32 @@
+import '../auth.form.scss'
 
  const Register = () => {
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Enter your username" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="username">Email</label>
+            <input type="text" id="email" name="email" placeholder="Enter your email address" />
+          </div>
+          <button type="submit" className="button primary-button">Register</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
