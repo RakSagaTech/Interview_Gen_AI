5e3bd3326aaba9113838b4ede23444eb1c97811b
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-07 16:05:47 +0530

    feat(auth): implement login form with SCSS styling and reusable button styles

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import '../auth.form.scss'
 
  const Login = () => {
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Enter your username" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" />
+          </div>
+          <button type="submit" className="button primary-button">Login</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
