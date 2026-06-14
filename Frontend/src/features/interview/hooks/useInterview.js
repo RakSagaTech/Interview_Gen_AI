3e3e9b3347034f6e44a0cddc4f5ed7acd152be7f
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-06-14 20:44:26 +0530

    feat(interview): add useInterview hook for report generation and retrieval

---
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -0,0 +1,52 @@
+import {useContext} from 'react'
+import {getAllInterviewReports, generateInterviewReport, getInterviewReportById} from '../services/interview.api'
+import {InterviewContext} from '../context/interview.context'
+
+export const useInterview = () =>{
+
+  const context = useContext(InterviewContext)
+
+  if(!context){
+    throw new Error("useInterview must be used within an InterviewProvider")
+  }
+
+  const {loading, setLoading, report, setReport, reports, setReports} = context 
+
+  const generateReport = async({jobDescription, selfDescription, resumeFile}) =>{
+    setLoading(true)
+    try{
+      const response = await generateInterviewReport({jobDescription, selfDescription, resumeFile})
+      setReport(response.interviewReport)
+    }catch(err){
+      console.log(err)
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  const getReportById = async(interviewId) =>{
+    setLoading(true)
+      try{
+        const response = await getInterviewReportById(interviewId)
+        setReport(response.interviewReport)
+      }catch(err){
+        console.log(err)
+      }finally{
+        setLoading(false)
+      }
+    }
+
+    const getReports = async() =>{
+      setLoading(true)
+      try{
+        const response = await getAllInterviewReports()
+        setReports(response.interviewReports)
+      }catch(err){
+        console.log(err)
+      }finally{
+        setLoading(false)
+      }
+    }
+
+    return {loading, report, reports, generateReport, getReportById, getReports}
+}
